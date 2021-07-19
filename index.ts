76b2acee945db744b04d15978d207135a6d3e072
Cody Bontecou <bontecouc@gmail.com>
2021-07-18 21:21:46 -1000

    simple script to supplement blog post at https://codybontecou.com/post-to-reddit-with-nodejs-and-typescript.html

---
+++ index.ts
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const snoowrap = require('snoowrap')
+require('dotenv').config()
+
+const config = {
+  username: process.env.username,
+  password: process.env.password,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+}
+
+function postLink(title: string, link: string, subreddit: string) {
+  const r = new snoowrap({
+    userAgent: 'Whatever',
+    clientId: config.clientId,
+    clientSecret: config.clientSecret,
+    username: config.username,
+    password: config.password,
+  })
+  r.getSubreddit(subreddit).submitLink({
+    title: title,
+    url: link,
+    sendReplies: true,
+  })
+}
+
+postLink('Testing', 'https://codybontecou.com', 'hotspringers')
